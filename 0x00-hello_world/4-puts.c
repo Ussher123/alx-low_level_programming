6849074bad3c6bb9e2bba1989da1dc98c917658c
Ussher123 <kingofusshers71@gmail.com>
2023-02-16 04:55:21 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #inlcude <stdio.h>
 /**
- * main -A program that prints a line with puts function
+ * main - A program that prints a line with puts function
  * Return:0
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
