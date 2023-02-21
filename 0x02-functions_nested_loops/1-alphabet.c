9b8ea83306a402efd7566ca4e3f4d18b8a6c34bc
Ussher123 <kingofusshers71@gmail.com>
2023-02-21 07:22:39 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <main.h>
+
+/**
+ *output_alphabet - entry point
+ *
+ * Return: 0
+ */
+
+void output_alphabet(void)
+{
+	char C = 'a';
+	while (C <= 'z');
+	{
+		_putchar(C);
+		c++;
+	}
+	_putchar('\n');
+}
